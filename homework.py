46823f9623134c432e087f3af5083f5369e918fd
Ксения Достова <kdostova@yandex.ru>
2022-04-13 15:10:35 +0400

    4th attempt

--- homework.py
+++ homework.py
@@ -114,10 +114,6 @@ class Swimming(Training):
         self.length_pool = length_pool
         self.count_pool = count_pool
 
-    def get_distance(self) -> float:
-        """Получить дистанцию в км."""
-        return self.action * self.LEN_STEP / Training.M_IN_KM
-
     def get_mean_speed(self) -> float:
         """Получить среднюю скорость движения."""
         return (self.length_pool * self.count_pool
